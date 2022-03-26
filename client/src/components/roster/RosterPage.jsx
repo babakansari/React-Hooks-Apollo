956891fd75e56994c83d74143b341ac9aa65aabd
babak.ansari <babak.ansari@cognizant.com>
2022-03-26 13:40:11 -0400

    Investigate Glide Data Grid

--- client/src/components/roster/RosterPage.jsx
+++ client/src/components/roster/RosterPage.jsx
@@ -1,13 +1,40 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import {
+  GridCellKind,
+  DataEditorContainer,
+  DataEditor
+} from "@glideapps/glide-data-grid";
 
-function RostersPage () {
-  return  (
+const columns = [
+  { title: "Number", width: 200 },
+  { title: "Square", width: 200 }
+];
+
+function getData([col, row]) {
+  let n;
+  if (col === 0) {
+    n = row;
+  } else if (col === 1) {
+    n = row * row;
+  } else {
+    throw new Error("This should not happen");
+  }
+  return {
+    kind: GridCellKind.Number,
+    data: n,
+    displayData: n.toString(),
+    allowOverlay: false
+  };
+}
+
+function RostersPage() {
+  return (
     <div>
-      <Typography variant="h2">Rosters</Typography>
-
+      <DataEditorContainer width={500} height={500}>
+        <DataEditor getCellContent={getData} columns={columns} rows={9000} />
+      </DataEditorContainer>
     </div>
   );
 }
+
 
 export default RostersPage;
