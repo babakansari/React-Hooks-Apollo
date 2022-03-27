490b42692a389c2285bff851b05a71ff32284d52
babak.ansari <babak.ansari@cognizant.com>
2022-03-27 19:32:10 -0400

    Calculate time elapse to generate data

--- client/src/components/roster/RosterPage.jsx
+++ client/src/components/roster/RosterPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDataGrid from "react-data-grid";
 import createRowData, { columns } from "./createRowData";
 
-const ROW_COUNT = 5000;
+const ROW_COUNT = 200000;
 const rows = createRowData(ROW_COUNT);
 
 function RostersPage() {
