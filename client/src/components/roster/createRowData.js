0daef8cc73c556c2f570c5314ddce939ada7aa98
babak.ansari <babak.ansari@cognizant.com>
2022-03-27 19:06:49 -0400

    Code clean up

--- client/src/components/roster/createRowData.js
+++ client/src/components/roster/createRowData.js
@@ -28,6 +28,75 @@ function createFakeRow(index) {
   };
 }
 
+const COLUMN_WIDTH = 140;
+
+export const columns = [
+  {
+    key: "id",
+    name: "ID",
+    frozen: true,
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "title",
+    name: "Title",
+    frozen: true,
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "firstName",
+    name: "First Name",
+    frozen: true,
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "lastName",
+    name: "Last Name",
+    frozen: true,
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "email",
+    name: "Email",
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "street",
+    name: "Street",
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "zipCode",
+    name: "ZipCode",
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "date",
+    name: "Date",
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "bs",
+    name: "bs",
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "catchPhrase",
+    name: "Catch Phrase",
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "companyName",
+    name: "Company Name",
+    width: COLUMN_WIDTH
+  },
+  {
+    key: "sentence",
+    name: "Sentence",
+    width: COLUMN_WIDTH
+  }
+];
+
 export default function createRowData(count) {
   return [...Array(count).keys()].map(i => createFakeRow(i));
 }
