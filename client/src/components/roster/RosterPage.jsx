b2b54f7bbe6a6cf56de007b73c29781580454aed
babak.ansari <babak.ansari@cognizant.com>
2022-03-27 19:07:26 -0400

    Code clean up glide grid

--- client/src/components/roster/RosterPage.jsx
+++ client/src/components/roster/RosterPage.jsx
@@ -1,102 +1,21 @@
 import {
-  GridCellKind,
   DataEditorContainer,
   DataEditor
 } from "@glideapps/glide-data-grid";
 import * as React from 'react';
-
-const columns = [
-  { title: "id",          id: "id",          width: 50 },
-  { title: "avartar",     id: "avartar",     width: 100 },
-  { title: "county",      id: "county",      width: 100 },
-  { title: "email",       id: "email",       width: 200 },
-  { title: "title",       id: "title",       width: 200 },
-  { title: "firstName",   id: "firstName",   width: 100 },
-  { title: "lastName",    id: "lastName",    width: 100 },
-  { title: "street",      id: "street",      width: 100 },
-  { title: "zipCode",     id: "zipCode",     width: 100 },
-  { title: "date",        id: "date",        width: 100 },
-  { title: "bs",          id: "bs",          width: 100 },
-  { title: "catchPhrase", id: "catchPhrase", width: 100 },
-  { title: "companyName", id: "companyName", width: 100 },
-  { title: "sentence",    id: "sentence",    width: 100 },
-  { title: "words",       id: "words",       width: 100 }
-];
-
-function makeid(length) {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
- return result;
-}
-
-function createFakeRow(index) {
-  return {
-    id: index,
-    avartar: makeid(50),
-    county: makeid(50),
-    email: makeid(50),
-    title: makeid(50),
-    firstName: makeid(50),
-    lastName: makeid(50),
-    street: index,
-    zipCode: index,
-    date:  Date.now(),
-    bs: index,
-    catchPhrase: index,
-    companyName: index,
-    sentence: index,
-    words: index
-  };
-}
-
-function createRowData(count) {
-  return [...Array(count).keys()].map(i => createFakeRow(i));
-}
+import createRowData, {getData, columns} from "./createRowData";
 
 const ROW_COUNT = 5000;
 const data = createRowData(ROW_COUNT);
-function getData([col, row]) {
-  const dataRow = data[row];
-  // dumb but simple way to do this
-  const indexes = [
-    "id",
-    "avartar",
-    "county",
-    "email",
-    "title",
-    "firstName",
-    "lastName",
-    "street",
-    "zipCode",
-    "date",
-    "bs",
-    "catchPhrase",
-    "companyName",
-    "sentence",
-    "words",
-  ];
-  const d = dataRow[indexes[col]]
-  return {
-      kind: GridCellKind.Text,
-      allowOverlay: false,
-      displayData: d.toString(),
-      data: d,
-  };
-}
 
 function RostersPage() {
   const getContent = React.useCallback((cell) => {
-    return getData(cell);
+    return getData(data, cell);
   }, []);
 
   return (
     <div>
-      <DataEditorContainer width={500} height={500}>
+      <DataEditorContainer width={1000} height={500}>
         <DataEditor getCellContent={getContent} columns={columns} rows={data.length} />
       </DataEditorContainer>
     </div>
