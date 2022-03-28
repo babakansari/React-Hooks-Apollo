a77a24ff32a9b706bc3f8e47ad2c6ae9ea882b14
babak.ansari <babak.ansari@cognizant.com>
2022-03-27 23:18:42 -0400

    Add styling to increase grid height

--- client/src/components/roster/RosterPage.jsx
+++ client/src/components/roster/RosterPage.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDataGrid from "react-data-grid";
 import createRowData, { getCols } from "./createRowData";
-
+import { makeStyles } from "@material-ui/styles"
 
 const rows = createRowData();
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: 600
+  }
+}));
+
 function RostersPage() {
-  
+  const classes = useStyles();
   return (
-    <div style = {{height:"100vh"}}>
+    <div>
       <ReactDataGrid
+        className={classes.root}
         columns={getCols()}
         rowGetter={i => rows[i]}
         rows={rows}
         minHeight={600}
+        enableCellSelect={true}
       />
     </div>
   );
