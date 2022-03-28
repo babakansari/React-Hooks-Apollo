ce07140453966d2e37e8676a9e5d1fcc96aa3d3c
babak.ansari <babak.ansari@cognizant.com>
2022-03-27 21:20:11 -0400

    Reduce number of rows

--- client/src/components/roster/RosterPage.jsx
+++ client/src/components/roster/RosterPage.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import ReactDataGrid from "react-data-grid";
 import createRowData, { columns } from "./createRowData";
 
-const ROW_COUNT = 200000;
+const ROW_COUNT = 100005;
 const rows = createRowData(ROW_COUNT);
 
 function RostersPage() {
   
   return (
-    <ReactDataGrid
-      columns={columns}
-      rowGetter={i => rows[i]}
-      rows={rows}
-      minHeight={600}
-    />
+    <div style = {{height:"100vh"}}>
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={i => rows[i]}
+        rows={rows}
+        minHeight={600}
+      />
+    </div>
   );
 }
 
