4040bb9b9e6aed2e7d6bd5a19064e71acbedd4e0
babak.ansari <babak.ansari@cognizant.com>
2022-03-29 18:48:55 -0400

    Implement 1:N relation from Rank to Crews

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,12 +1,20 @@
 import db from './db.js';
 
 const Query = {
+    rank: (root, {id}) => db.ranks.get(id),
     crew: (root, {id}) => db.rostering.get(id), 
     rostering: () => db.rostering.list()
 };
 
+// Many-to-one relation from Crew to Rank
 const Crew = {
     rank: (crew) => db.ranks.get(crew.rankId)
 }
 
-export default { Query, Crew };
+// One-to-many relation from Rank to Company
+const Rank = {
+    Crews: (rank) => db.rostering.list()
+                        .filter( (crew)=> crew.rankId === rank.id )
+}
+
+export default { Query, Crew, Rank };
