207b60fcd7a4f9277c3dbd394eb3c7589a3b3d2d
babak.ansari <babak.ansari@cognizant.com>
2022-03-31 17:06:00 -0400

    Add eslint

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import {  BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import React  from 'react';
 import { Container } from "@mui/material";
 import Header from "./common/Header";
 import HomePage from './common/HomePage'
