26d5ccdc01f5832cada5bf75b280c9e8a9d8106f
babak.ansari <babak.ansari@cognizant.com>
2022-04-01 09:25:38 -0400

    Modify Data Grid column and scroll to a cell

--- client/src/components/roster/RosterPage.jsx
+++ client/src/components/roster/RosterPage.jsx
@@ -9,14 +9,23 @@ const data = createRowData();
 const columns = getColumn();
 
 function RostersPage() {
+  const [cols, setCols] = React.useState(columns);
   const getContent = React.useCallback((cell) => {
     return getData(data, cell);
   }, []);
 
+  const gridRef = React.useRef(null);
+
   return (
     <div>
       <DataEditorContainer width={1000} height={600} >
-        <DataEditor getCellContent={getContent} columns={columns} rows={data.length} freezeColumns={4}/>
+        <DataEditor ref={gridRef} getCellContent={getContent} columns={cols} rows={data.length} freezeColumns={4} onHeaderClicked={(colIndex, event) =>{
+          gridRef.current.scrollTo(colIndex, 500);
+          const newColumns = [...columns];
+          newColumns[0].icon = "headerMarkdown";
+          setCols(newColumns);
+          console.log(`Col: ${colIndex}, Ref: ${JSON.stringify(gridRef.current.columns)}`);
+        }}/>
       </DataEditorContainer>
     </div>
   );
