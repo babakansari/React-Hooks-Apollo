61c019b834759ee5fa72a1cac1a454878808a980
Babak Ansari <babakansari@hotmail.com>
2022-06-09 21:52:26 -0400

    Use session manager for custom route

--- client/src/components/CustomRoute.js
+++ client/src/components/CustomRoute.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { SecureRoute } from '@okta/okta-react';
 import { getPathByLabel } from './common/MenuMap';
-import { useOktaAuth } from '@okta/okta-react';
-import { useBasicAuth } from './auth/BasicAuth';
+import { useSession } from './auth/SessionManager';
 
 export const CustomRoute = ({ label, component }) => {
-  const oktaAuth = useOktaAuth();
-  const basicAuth = useBasicAuth();
-  const isBasicAuthenticated = basicAuth.authState.isAuthenticated;
-  const isOktaAuthenticated = oktaAuth.authState && oktaAuth.authState.isAuthenticated;
+  const session = useSession();
 
-  return isBasicAuthenticated || isOktaAuthenticated
+  return session.isAuthenticated
     ? <Route path={getPathByLabel(label)} component={component} />
     : <SecureRoute path={getPathByLabel(label)} component={component} />;
 };
