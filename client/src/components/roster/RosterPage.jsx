3cccccdbdc145086d85fab5bef64240bda721fc9
babak.ansari <babak.ansari@cognizant.com>
2022-06-14 09:57:35 -0400

    Cleanup

--- client/src/components/roster/RosterPage.jsx
+++ client/src/components/roster/RosterPage.jsx
@@ -4,11 +4,7 @@ import {
 } from "@glideapps/glide-data-grid";
 import * as React from 'react';
 import createRowData, {getData, getColumn, getSearchData, ROW_COUNT} from "./createRowData";
-//import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync';
-//import { ScrollSync, ScrollSyncNode } from "scroll-sync-react";
-//import { useScrollSync } from "react-use-scroll-sync"
-//import useSyncScroll from 'react-use-sync-scroll';
-import {Slider, Grid, TextField, Button , Typography } from '@mui/material';
+import {Grid, TextField, Button , Typography } from '@mui/material';
 import { blue } from '@mui/material/colors';
 import * as Lodash from 'lodash';
 
@@ -20,14 +16,12 @@ function RostersPage() {
   const [foundRows, setFoundRows] = React.useState([]);
   const [totalFound, setTotalFound] = React.useState();
 
-  //const [y, setY] = React.useState(0);
   const [position, setPosition] = React.useState();
   const [scrollTo, SetScrollTo] = React.useState(0);
 
   const [firstRow, setFirstRow] = React.useState(-1);
 
   const getContent1 = React.useCallback((cell) => {
-    //console.log(`row: ${JSON.stringify(cell[1])}`);
     if(firstRow==-1){
       setFirstRow(cell[1]);
     }
@@ -35,29 +29,13 @@ function RostersPage() {
   }, [data, foundRows]);
 
   const getContent2 = React.useCallback((cell) => {
-    //console.log(`row: ${JSON.stringify(cell[1])}`);
     return getData(data, cell);
   }, [data, foundRows]);
 
   const gridRef1 = React.useRef(null);
   const gridRef2 = React.useRef(null);
-  const sliderRef = React.useRef(null);
-  const gridRefs = React.useRef([gridRef1, gridRef2]);
   const scrollRef = React.useRef(null);
   
-  // Use the hook
-  // useSyncScroll(gridRefs, { vertical: true, horizontal: false });
-
-
-  // useScrollSync([gridRef1, gridRef2], {
-  //   horizontal: true,
-  //   vertical: true,
-  //   proportional: true
-  // })
-
-  function valuetext(value) {
-    return `#${value}`;
-  }
 
   const getRowThemeOverride = React.useCallback((row) => {
       if( foundRows.indexOf(row.toString())>=0 ) {
@@ -110,46 +88,26 @@ function RostersPage() {
             top: range.y,
             height: range.height
           }));
-          //setY(range.y);
   }, [data]);
 
   const onScrollClicked = () => {
     let y = scrollTo;
-    // console.log(`top = ${position.top}, height = ${position.height}`);
-    const top = position.top;
-    const bottom = position.top + position.height-2;
+    // console.log(`y = ${y}, top = ${top}, bottom = ${bottom}`);
 
-    console.log(`y = ${y}, top = ${top}, bottom = ${bottom}`);
-
-    if(y>top){
-      // y = bottom+y-top-1;
+    if(y>position.top){
       y = position.height+y-3;
     }
-
-    // if(y>top && y<bottom){
-    //   y = bottom+y-top-1;
-    // } else if(y>bottom) {
-    //   y = bottom+y-top-1;
-    // }
-    console.log(`y' = ${y}`);
  
     //console.log(`Bounds = ${JSON.stringify(gridRef1.current.getBounds(1,2))}`);
     gridRef1.current.scrollTo(0,y);
-  
   }
 
   const onScrollTo = React.useCallback((e) => {
-    
     let y = parseInt(e.target.value.toUpperCase());
     if(isNaN(y)){
       y = 0;
     }
     SetScrollTo(y);
-    // var node = document.getElementsByClassName("dvn-scroller ");
-    // //var a = ReactDOM.findDOMNode(node);
-  
-    // console.log(`ref = ${JSON.stringify(node)}`);
-
   }, [position]);
 
 
@@ -167,26 +125,8 @@ function RostersPage() {
       </Grid>
       {/* <ScrollSync> */}
         <div>
-          <Grid item>
-            {/* <Slider
-              ref={sliderRef}
-              aria-label="Row"
-              defaultValue={0}
-              value={y}
-              getAriaValueText={valuetext}
-              valueLabelDisplay="auto"
-              step={1}
-              min={0}
-              max={ROW_COUNT}
-              orientation="vertical"
-              onChange={(e, val) =>{
-                gridRef.current.scrollTo(0, val);
-                setY(val);
-              }}
-            /> */}
-            
+          <Grid item>            
               <DataEditorContainer width={1000} height={292}>
-               {/* <ScrollSyncPane group="a" > */}
                   <DataEditor 
                       ref={gridRef1} 
                       scrollRef={scrollRef}
@@ -196,36 +136,22 @@ function RostersPage() {
                       rows={data.length} 
                       freezeColumns={4} 
                       getRowThemeOverride={getRowThemeOverride}
-
-                      // onVisibleRegionChanged={
-                      //   (range) =>{
-                      //     setPosition(({
-                      //       top: range.y,
-                      //       height: range.height
-                      //     }));
-                      //     setY(range.y);
-                      //   }
-                      // }
-
                       onHeaderClicked={(colIndex, event) =>{
                           const newColumns = [...columns];
                           newColumns[0].icon = newColumns[0].icon ? undefined : "headerMarkdown";
                           setCols(newColumns);
-                          // log(`Col: ${colIndex}, Ref: ${JSON.stringify(gridRef.current.columns)}`);
+
                         }
                       }
-
                       showSearch={true} 
                       getCellsForSelection={ cellsForSelection } 
                   />
-                {/* </ScrollSyncPane> */}
               </DataEditorContainer>
             
           </Grid>
           <Grid item>
             
               <DataEditorContainer width={1000} height={280} >
-                {/* <ScrollSyncPane group="a"> */}
                   <DataEditor 
                       ref={gridRef2} 
                       getCellContent={getContent2} 
@@ -233,33 +159,21 @@ function RostersPage() {
                       rows={data.length} 
                       freezeColumns={4} 
                       getRowThemeOverride={getRowThemeOverride}
-                      
-                      // onVisibleRegionChanged={
-                      //   (range) =>{
-                      //     //gridRef1.current.scrollTo(0, range.y);
-                      //     //gridRef1.current.setAttribute("BABAK", "helloButton");
-                      //     //console.log(`gridRef1=${JSON.stringify(gridRef2.current)}`);
-                      //     //setY(range.y);
-                      //   }
-                      // }
 
                       onHeaderClicked={(colIndex, event) =>{
                           const newColumns = [...columns];
                           newColumns[0].icon = newColumns[0].icon ? undefined : "headerMarkdown";
                           setCols(newColumns);
-                          // log(`Col: ${colIndex}, Ref: ${JSON.stringify(gridRef.current.columns)}`);
                         }
                       }
 
                       showSearch={true} 
                       getCellsForSelection={ cellsForSelection } 
                   />
-                {/* </ScrollSyncPane> */}
               </DataEditorContainer>
             
           </Grid>
         </div>
-      {/* </ScrollSync> */}
     </Grid>
   );
 }
