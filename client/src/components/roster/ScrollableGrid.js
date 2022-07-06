4a6dcd62da47d86c5d7b7ff249636b946b56c33f
babak.ansari <babak.ansari@cognizant.com>
2022-07-06 11:36:30 -0400

    Add context menu to grid

--- client/src/components/roster/ScrollableGrid.js
+++ client/src/components/roster/ScrollableGrid.js
@@ -4,6 +4,8 @@ import {
     DataEditor
   } from "@glideapps/glide-data-grid";
 import { GridEvent, GridPosition } from './ScrollableGridTypes';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const ScrollableGridImpl = (props, forwardedRef) => {
     const gridRef = React.useRef(null);
@@ -19,6 +21,10 @@ const ScrollableGridImpl = (props, forwardedRef) => {
     const gridHeight = visibleRows * (rowHeight + 1) + headerHeight + 1 + epsilon;
     const onScrollEventRef = React.useRef(null);
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorPos, setAnchorPos] = React.useState({x:0,y:0});
+    const open = Boolean(anchorEl);
+
     const onVisibleRegionChanged = React.useCallback( ( range, tx, ty ) => {
         if(!onScrollEventRef){
             return;
@@ -33,7 +39,22 @@ const ScrollableGridImpl = (props, forwardedRef) => {
         setPosition(currentPosition);
 
     }, [onScrollEventRef, forwardedRef]);
+
+    const onCellContextMenu = (cell, event) => {
+        console.log(`cell=${JSON.stringify(cell)}, \r\n event = ${JSON.stringify(event)}`);
+        setAnchorPos({ x: event.bounds.x, y: event.bounds.y});
+
+        const bounds = gridRef.current.getBounds(cell[0], cell[1]);
+        console.log(`bound = ${JSON.stringify(bounds)}`);
+        
+        setAnchorEl(event);
+        event.preventDefault();
+    };
     
+    const onCellClicked = (cell) => {
+        //alert(`cell=${JSON.stringify(cell)}}`);
+    };
+
     const ScrollTo = React.useCallback( (top, left) => {
         let y = (top>position.top) ? position.height+top-3 : top; 
         gridRef.current.scrollTo(0,y);
@@ -52,22 +73,56 @@ const ScrollableGridImpl = (props, forwardedRef) => {
         }),
         [ ScrollTo ]
     );
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
     
     return (
-        <DataEditorContainer width={1000} height={gridHeight}>
-            <DataEditor 
-                ref={gridRef} 
-                getCellContent={props.getCellContent} 
-                columns={props.columns} 
-                rows={props.rows}
-                onVisibleRegionChanged={ onVisibleRegionChanged }
-                freezeColumns={props.freezeColumns}
-                getRowThemeOverride={props.getRowThemeOverride}
-                getCellsForSelection={props.getCellsForSelection}
-                rowHeight={rowHeight}
-                headerHeight={headerHeight}
-            />
-        </DataEditorContainer>
+        <div onContextMenu={(e)=> e.preventDefault()} >
+            <DataEditorContainer width={1000} height={gridHeight}>
+                <DataEditor 
+                    ref={gridRef} 
+                    getCellContent={props.getCellContent} 
+                    columns={props.columns} 
+                    rows={props.rows}
+                    onVisibleRegionChanged={ onVisibleRegionChanged }
+                    freezeColumns={props.freezeColumns}
+                    getRowThemeOverride={props.getRowThemeOverride}
+                    getCellsForSelection={props.getCellsForSelection}
+                    rowHeight={rowHeight}
+                    headerHeight={headerHeight}
+                    onCellContextMenu={onCellContextMenu}
+                    onCellClicked={onCellClicked}
+                    
+                />
+            </DataEditorContainer>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                'aria-labelledby': 'basic-button',
+                }}
+                anchorOrigin={{
+                    vertical: anchorPos.y,
+                    horizontal: anchorPos.x,
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                // style={{
+                //     top: -100,
+                //     left: 100,
+                // }}
+                >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </div>
     );
 }
 
